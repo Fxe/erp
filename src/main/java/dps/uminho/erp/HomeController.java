6bac3f44296253d85d0e41629a89f7c559086ef4
Bruno <bruno16@MacBook-Pro-de-Bruno-3.local>
2013-04-28 18:48:13 +0100

    um print

--- src/main/java/dps/uminho/erp/HomeController.java
+++ src/main/java/dps/uminho/erp/HomeController.java
@@ -29,6 +29,8 @@ public class HomeController {
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
+		System.out.println("edit here !");
+		
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
